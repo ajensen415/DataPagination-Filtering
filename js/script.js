a3e1bbc8e456b5a69c2fcc519c299a7de7f3b720
ajensen415 <ajensen415@yahoo.com>
2021-06-17 17:37:36 -0400

    code comments
    added comments to my cod/removed console.log() testing.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+//This function will display a page of 9 students & include each individual's personal info.
 function showPage (list, page) {
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
@@ -24,8 +7,6 @@ function showPage (list, page) {
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i ++ ) {
       if (i >= startIndex && i < endIndex) {
-         //console.log(list[i].picture.thumbnail);
-         //console.log('test');
          const li = document.createElement('li');
          li.className = 'student-item cf';
          studentList.appendChild(li);
@@ -61,11 +42,7 @@ function showPage (list, page) {
    }
 }
 
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+//This function will create & append the pagination buttons to allow users to navigate to different pages.
 function addPagination(list) {
    const buttonCount = Math.ceil(list.length / 9);
    const linkList = document.querySelector('.link-list');
@@ -91,7 +68,7 @@ function addPagination(list) {
    });
 }
 
-// Call functions
+//Call functions
 showPage(data,1);
 addPagination(data);
 
